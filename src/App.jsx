3670ba1a5a66cafffd00e6925fbb35e3b9e6f4ca
Nhat Hoang <johnhoanghtx@gmail.com>
2022-07-13 01:12:41 -0500

    Adding Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -8,6 +8,27 @@ import Detail from "./Detail";
 import Cart from "./Cart";
 
 export default function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(id, sku) {
+    setCart((items) => {
+      const itemInCart = items.find((i) => itemInCart.sku === sku);
+      if (itemInCart) {
+        return items.map((i) =>
+          i.sku === sku ? { ...i, quantity: i.quantity + 1 } : i
+        );
+      } else {
+        return [...items, { id, sku, quantity: 1 }]
+      }
+    });
+  }
+
+  function updateQuantity(sku, quantity) {
+    setCart((items) => {
+      return items.map((i) => i.sku === sku ? {...i, quantity} : i)
+    })
+  }
+
   return (
     <>
       <div className="content">
@@ -16,8 +37,8 @@ export default function App() {
           <Routes>
             <Route path="/" element={<h1>Welcome to Carved Rock Fitness</h1>} />
             <Route path="/:category" element={<Products></Products>} />
-            <Route path="/:category/:id" element={<Detail></Detail>} />
-            <Route path="/cart" element={<Cart></Cart>} />
+            <Route path="/:category/:id" element={<Detail addToCart={addToCart}></Detail>} />
+            <Route path="/cart" element={<Cart cart={cart} updateQuantity={updateQuantity}></Cart>} />
           </Routes>
         </main>
       </div>
