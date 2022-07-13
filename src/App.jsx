77163fe9bc103ec28adb7720970d8485e307c362
Nhat Hoang <johnhoanghtx@gmail.com>
2022-07-13 12:09:58 -0500

    Added Cart and Checkout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -6,27 +6,45 @@ import Products from "./Products";
 import { Routes, Route } from "react-router-dom";
 import Detail from "./Detail";
 import Cart from "./Cart";
+import Checkout from "./Checkout";
 
 export default function App() {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => {
+    try {
+      return JSON.parse(localStorage.getItem("cart")) ?? [];
+    } catch {
+      console.error("The cart could not be parsed into JSON.");
+      return [];
+    }
+  });
+
+  useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
 
   function addToCart(id, sku) {
     setCart((items) => {
-      const itemInCart = items.find((i) => itemInCart.sku === sku);
+      const itemInCart = items.find((i) => i.sku === sku);
       if (itemInCart) {
+        // Return new array with the matching item replaced
         return items.map((i) =>
           i.sku === sku ? { ...i, quantity: i.quantity + 1 } : i
         );
       } else {
-        return [...items, { id, sku, quantity: 1 }]
+        // Return new array with the new item appended
+        return [...items, { id, sku, quantity: 1 }];
       }
     });
   }
 
   function updateQuantity(sku, quantity) {
     setCart((items) => {
-      return items.map((i) => i.sku === sku ? {...i, quantity} : i)
-    })
+      return quantity === 0
+        ? items.filter((i) => i.sku !== sku)
+        : items.map((i) => (i.sku === sku ? { ...i, quantity } : i));
+    });
+  }
+
+  function emptyCart() {
+    setCart([]);
   }
 
   return (
@@ -36,9 +54,19 @@ export default function App() {
         <main>
           <Routes>
             <Route path="/" element={<h1>Welcome to Carved Rock Fitness</h1>} />
-            <Route path="/:category" element={<Products></Products>} />
-            <Route path="/:category/:id" element={<Detail addToCart={addToCart}></Detail>} />
-            <Route path="/cart" element={<Cart cart={cart} updateQuantity={updateQuantity}></Cart>} />
+            <Route path="/:category" element={<Products />} />
+            <Route
+              path="/:category/:id"
+              element={<Detail addToCart={addToCart} />}
+            />
+            <Route
+              path="/cart"
+              element={<Cart cart={cart} updateQuantity={updateQuantity} />}
+            />
+            <Route
+              path="/checkout"
+              element={<Checkout cart={cart} emptyCart={emptyCart} />}
+            />
           </Routes>
         </main>
       </div>
--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import useFetchAll from "./services/useFetchAll";
 import Spinner from "./Spinner";
 
 export default function Cart({ cart, updateQuantity }) {
+  const navigate = useNavigate();
   const urls = cart.map((i) => `products/${i.id}`);
   const { data: products, loading, error } = useFetchAll(urls);
 
@@ -42,10 +44,24 @@ export default function Cart({ cart, updateQuantity }) {
   if (loading) return <Spinner />;
   if (error) throw error;
 
+  const numItemsInCart = cart.reduce((total, item) => total + item.quantity, 0);
+
   return (
     <section id="cart">
-      <h1>Cart</h1>
+      <h1>
+        {numItemsInCart === 0
+          ? "Your cart is empty"
+          : `${numItemsInCart} Item${numItemsInCart > 1 ? "s" : ""} in My Cart`}
+      </h1>
       <ul>{cart.map(renderItem)}</ul>
+      {cart.length > 0 && (
+        <button
+          className="btn btn-primary"
+          onClick={() => navigate("/checkout")}
+        >
+          Checkout
+        </button>
+      )}
     </section>
   );
 }
